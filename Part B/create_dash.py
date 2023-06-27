c32ba0ef3112b075eff23d613b887899645b40a3
Gony97 <82442657+Gony97@users.noreply.github.com>
2023-06-27 20:23:37 +0300

    adding community table to dash

--- Part B/create_dash.py
+++ Part B/create_dash.py
@@ -1,5 +1,5 @@
 import dash
-from dash import dcc, callback, Output, Input
+from dash import dcc, callback, Output, Input, dash_table
 from dash import html
 import pandas as pd
 from create_graphs import generate_graph
@@ -7,7 +7,7 @@ import plotly.io as pio
 import json
 import re
 
-start = 'C:/Users/gony/Desktop/'
+start = 'C:/Users/gony/Desktop/data/'
 
 dates_9_11 = [f'{i}.09.2001' for i in range(6, 27)]
 dates_iraq = [f'{i}.03.2003' for i in range(18, 32)] + [f'{i}.04.2003' for i in range(1, 4)]
@@ -15,6 +15,9 @@ dates_katrina = [f'{i}.08.2005' for i in range(18, 32)] + [f'{i}.09.2005' for i 
 dates_binladin = [f'{i}.04.2011' for i in range(27, 31)] + [f'{i}.05.2011' for i in range(1, 18)]
 dates_boston = [f'{i}.04.2013' for i in range(11, 31)]
 
+df = pd.read_csv(start + 'nodes_centrality_9_11_2001/centrality_community_6.09.2001.csv')
+df.sort_values(['density'], ascending=False, inplace=True)
+df = df[['id', 'top_degree_cent']][:10]
 
 
 def read_html(path):
@@ -26,11 +29,24 @@ def read_html(path):
     return pio.from_json(json.dumps(plotly_json))
 
 
-fig_9_11 = read_html(start + 'data/graphs/9_11_2001/6.09.2001_graph.html')
-fig_iraq = read_html(start + 'data/graphs/iraq_3_20_2003/18.03.2003_graph.html')
-fig_katrina = read_html(start + 'data/graphs/katrina_08_2005/2.08.2005_graph.html')
-fig_binladin = read_html(start + 'data/graphs/binladin_02_05_2011/1.05.2011_graph.html')
-fig_boston = read_html(start + 'data/graphs/boston_15_04_2013/11.04.2013_graph.html')
+def generate_table(dataframe, name, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+fig_9_11 = read_html(start + 'graphs/9_11_2001/6.09.2001_graph.html')
+fig_iraq = read_html(start + 'graphs/iraq_3_20_2003/18.03.2003_graph.html')
+fig_katrina = read_html(start + 'graphs/katrina_08_2005/2.08.2005_graph.html')
+fig_binladin = read_html(start + 'graphs/binladin_02_05_2011/1.05.2011_graph.html')
+fig_boston = read_html(start + 'graphs/boston_15_04_2013/11.04.2013_graph.html')
 
 app = dash.Dash(__name__)
 app.layout = html.Div(
@@ -42,11 +58,15 @@ app.layout = html.Div(
         html.H2(children='graphs for the event of 9/11 terror attack'),
         dcc.RangeSlider(-5, 15, 1, value=[-5], id='range-slider_9_11'),
         html.H3(children=dates_9_11[0], id='title_9_11'),
-        dcc.Graph(
-            figure=fig_9_11,
-            id='graph_9_11',
-
-        ),
+        html.Div([html.Div(dcc.Graph(
+                            figure=fig_9_11,
+                            id='graph_9_11'),
+                            style={'width': '45%', 'display': 'inline-block'}),
+                    html.Div(dash_table.DataTable(
+                            id='table-container',
+                            columns=[{"name": i, "id": i} for i in df],
+                            data=df.to_dict('records'), style_table={'height': 200, 'overflowX': 'auto'},
+                ), style={'width': '45%', 'display': 'inline-block'})]),
         html.H2(children='graphs for the event of invading Iraq at March 2003'),
         dcc.RangeSlider(-5, 15, 1, value=[-5], id='range-slider_iraq'),
         html.H3(children=dates_iraq[0], id='title_iraq'),
@@ -84,13 +104,18 @@ app.layout = html.Div(
 @callback(
     Output(component_id='graph_9_11', component_property='figure'),
     Output(component_id='title_9_11', component_property='children'),
-    Input(component_id='range-slider_9_11', component_property='value')
+    Output(component_id='table-container', component_property='data'),
+    Input(component_id='range-slider_9_11', component_property='value'),
+
 )
 def update_9_11_graph(val_chosen):
-    val_chosen = val_chosen[0]+5
-    path = f'{start}data/graphs/9_11_2001/{dates_9_11[val_chosen]}_graph.html'
+    val_chosen = val_chosen[0] + 5
+    path = f'{start}graphs/9_11_2001/{dates_9_11[val_chosen]}_graph.html'
     fig = read_html(path)
-    return fig, dates_9_11[val_chosen]
+    df = pd.read_csv(start + f'nodes_centrality_9_11_2001/centrality_community_{dates_9_11[val_chosen]}.csv')
+    df.sort_values(['density'], ascending=False, inplace=True)
+    df = df[['id', 'top_degree_cent']][:10]
+    return fig, dates_9_11[val_chosen], df.to_dict('records'),
 
 
 @callback(
@@ -99,8 +124,8 @@ def update_9_11_graph(val_chosen):
     Input(component_id='range-slider_iraq', component_property='value')
 )
 def update_iraq_graph(val_chosen):
-    val_chosen = val_chosen[0]+5
-    path = f'{start}data/graphs/iraq_3_20_2003/{dates_iraq[val_chosen]}_graph.html'
+    val_chosen = val_chosen[0] + 5
+    path = f'{start}graphs/iraq_3_20_2003/{dates_iraq[val_chosen]}_graph.html'
     fig = read_html(path)
     return fig, dates_iraq[val_chosen]
 
@@ -111,8 +136,8 @@ def update_iraq_graph(val_chosen):
     Input(component_id='range-slider_katrina', component_property='value')
 )
 def update_katrina_graph(val_chosen):
-    val_chosen = val_chosen[0]+5
-    path = f'{start}data/graphs/katrina_08_2005/{dates_katrina[val_chosen]}_graph.html'
+    val_chosen = val_chosen[0] + 5
+    path = f'{start}graphs/katrina_08_2005/{dates_katrina[val_chosen]}_graph.html'
     fig = read_html(path)
     return fig, dates_katrina[val_chosen]
 
@@ -123,8 +148,8 @@ def update_katrina_graph(val_chosen):
     Input(component_id='range-slider_binladin', component_property='value')
 )
 def update_binladin_graph(val_chosen):
-    val_chosen = val_chosen[0]+5
-    path = f'{start}data/graphs/binladin_02_05_2011/{dates_binladin[val_chosen]}_graph.html'
+    val_chosen = val_chosen[0] + 5
+    path = f'{start}graphs/binladin_02_05_2011/{dates_binladin[val_chosen]}_graph.html'
     fig = read_html(path)
     return fig, dates_binladin[val_chosen]
 
@@ -135,7 +160,7 @@ def update_binladin_graph(val_chosen):
     Input(component_id='range-slider_boston', component_property='value')
 )
 def update_boston_graph(val_chosen):
-    val_chosen = val_chosen[0]+5
+    val_chosen = val_chosen[0] + 5
     path = f'{start}data/graphs/boston_15_04_2013/{dates_boston[val_chosen]}_graph.html'
     fig = read_html(path)
     return fig, dates_boston[val_chosen]
